934d3f37b9e2f7147a64f9d6db2491232e2d0f47
ranqingwen <ranqingwen@gmail.com>
2026-03-22 12:45:19 +0800

    2026.3.22

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -8,7 +8,7 @@
 # 后台IP设置
 export Ipv4_ipaddr="192.168.23.254"            # 修改openwrt后台地址(填0为关闭)
 export Netmask_netm="255.255.255.0"         # IPv4 子网掩码（默认：255.255.255.0）(填0为不作修改)
-export Op_name="Lede"                # 修改主机名称为Lede(填0为不作修改)
+export Op_name="OpenWrt"                # 修改主机名称为Lede(填0为不作修改)
 
 # 内核和系统分区大小(不是每个机型都可用)
 export Kernel_partition_size="32"            # 内核分区大小,每个机型默认值不一样 (填写您想要的数值,默认一般16,数值以MB计算，填0为不作修改),如果你不懂就填0
@@ -31,7 +31,7 @@ export Enable_IPV6_function="1"             # 编译IPV6固件(1为启用命令,
 export Enable_IPV4_function="0"             # 编译IPV4固件(1为启用命令,填0为不作修改)(如果跟Enable_IPV6_function一起启用命令的话,此命令会自动关闭)
 
 # 替换OpenClash的源码(默认master分支)
-export OpenClash_branch="0"                 # OpenClash的源码分别有【master分支】和【dev分支】(填0为关闭,填1为使用master分支,填2为使用dev分支,填入1或2的时候固件自动增加此插件)
+export OpenClash_branch="1"                 # OpenClash的源码分别有【master分支】和【dev分支】(填0为关闭,填1为使用master分支,填2为使用dev分支,填入1或2的时候固件自动增加此插件)
 
 # 个性签名,默认增加年月日[$(TZ=UTC-8 date "+%Y.%m.%d")]
 export Customized_Information="Lede by ranqw $(TZ=UTC-8 date "+%Y.%m.%d")"  # 个性签名,你想写啥就写啥，(填0为不作修改)
@@ -44,8 +44,8 @@ export Password_free_login="1"               # 设置首次登录后台密码为
 
 # 增加AdGuardHome插件和核心
 export AdGuardHome_Core="0"                  # 编译固件时自动增加AdGuardHome插件和AdGuardHome插件核心,需要注意的是一个核心20多MB的,小闪存机子搞不来(1为启用命令,填0为不作修改)
-# 拉取应用过滤(OAF)源码
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# 增加应用过滤插件
+export Install_OAF="1"      # 是否安装应用过滤(OAF)插件 (1为安装, 0为不安装)
 
 # 开启NTFS格式盘挂载
 export Automatic_Mount_Settings="1"          # 编译时加入开启NTFS格式盘挂载的所需依赖(1为启用命令,填0为不作修改)
